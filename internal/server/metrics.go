787571fb8a9a8d475119f98c77645400a91b1bcf
Dane O'Connor <dane.oconnor@gmail.com>
2020-10-19 14:16:08 -0400

    fix whitespace

--- internal/server/metrics.go
+++ internal/server/metrics.go
@@ -1,12 +1,12 @@
 package server
 
 import (
-	"context"
-	"net/http"
-	"time"
+  "context"
+  "net/http"
+  "time"
 
-	"github.com/golang/glog"
-	"github.com/livepeer/stream-tester/internal/metrics"
+  "github.com/golang/glog"
+  "github.com/livepeer/stream-tester/internal/metrics"
 )
 
 type MetricsServer struct {
@@ -14,27 +14,27 @@ type MetricsServer struct {
 
 // NewStreamerServer creates new MetricsServer
 func NewMetricsServer() *MetricsServer {
-	return &MetricsServer{}
+  return &MetricsServer{}
 }
 
 // Starts metric server
 // blocks until exit
 func (s *MetricsServer) Start(ctx context.Context, bindAddr string) {
-	mux := http.NewServeMux()
-	mux.Handle("/metrics", metrics.Exporter)
+  mux := http.NewServeMux()
+  mux.Handle("/metrics", metrics.Exporter)
 
-	srv := &http.Server{
-		Addr:    bindAddr,
-		Handler: mux,
-	}
+  srv := &http.Server{
+    Addr:    bindAddr,
+    Handler: mux,
+  }
 
-	go func() {
-		<-ctx.Done()
-		c, _ := context.WithTimeout(context.Background(), time.Second)
-		glog.Infof("Shutting down metrics server")
-		srv.Shutdown(c)
-	}()
+  go func() {
+    <-ctx.Done()
+    c, _ := context.WithTimeout(context.Background(), time.Second)
+    glog.Infof("Shutting down metrics server")
+    srv.Shutdown(c)
+  }()
 
-	glog.Info("Metrics server listening on ", bindAddr)
-	srv.ListenAndServe()
+  glog.Info("Metrics server listening on ", bindAddr)
+  srv.ListenAndServe()
 }
