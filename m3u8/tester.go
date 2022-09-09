a80fad539a7f24942b7e6f6dca3a1520ae89d742
Victor Elias <victorgelias@gmail.com>
2022-09-09 15:21:04 -0300

    m3u8: Expose InitCensus func

--- m3u8/tester.go
+++ m3u8/tester.go
@@ -3,11 +3,18 @@ package m3u8
 import (
 	"context"
 	"fmt"
+	"os"
 	"time"
 
 	"github.com/livepeer/go-api-client"
+	"github.com/livepeer/stream-tester/internal/metrics"
 	"github.com/livepeer/stream-tester/internal/testers"
 )
+
+func InitCensus(service, version string) {
+	hostname, _ := os.Hostname()
+	metrics.InitCensus(hostname, version, service)
+}
 
 func Check(ctx context.Context, url string, expectedDuration time.Duration) error {
 	downloader := testers.NewM3utester2(ctx, url, false, false, false, false, 5*time.Second, nil, false)
