834754b3932bd7ae9000d1e44882f55caee2b3c1
Victor Elias <victorgelias@gmail.com>
2022-09-12 17:36:18 -0300

    m3u8: Receive expected profiles as argument

--- m3u8/tester.go
+++ m3u8/tester.go
@@ -16,12 +16,12 @@ func InitCensus(service, version string) {
 	metrics.InitCensus(hostname, version, service)
 }
 
-func Check(ctx context.Context, url string, expectedDuration time.Duration) error {
+func Check(ctx context.Context, url string, profiles []api.Profile, expectedDuration time.Duration) error {
 	downloader := testers.NewM3utester2(ctx, url, false, false, false, false, 5*time.Second, nil, false)
 	<-downloader.Done()
 	stats := downloader.VODStats()
-	if len(stats.SegmentsNum) != len(api.StandardProfiles)+1 {
-		return fmt.Errorf("number of renditions doesn't match (has %d should %d)", len(stats.SegmentsNum), len(api.StandardProfiles)+1)
+	if len(stats.SegmentsNum) != len(profiles)+1 {
+		return fmt.Errorf("number of renditions doesn't match (has %d should %d)", len(stats.SegmentsNum), len(profiles)+1)
 	}
 	if ok, ers := stats.IsOk(expectedDuration, false); !ok {
 		return fmt.Errorf("playlist not ok: %s", ers)
