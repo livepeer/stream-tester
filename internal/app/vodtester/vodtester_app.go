c77fa4533afb98a2f86235189bea55bb97ce74c0
Victor Elias <victorgelias@gmail.com>
2023-01-24 18:45:50 -0300

    vodtester: Call checkPlayback from all the right places

--- internal/app/vodtester/vodtester_app.go
+++ internal/app/vodtester/vodtester_app.go
@@ -139,8 +139,15 @@ func (vt *vodTester) uploadViaUrlTester(vodImportUrl string, taskPollDuration ti
 
 	if err != nil {
 		glog.Errorf("Error processing asset assetId=%s taskId=%s", importAsset.ID, importTask.ID)
+		return nil, fmt.Errorf("error waiting for asset processing: %w", err)
 	}
-	return importAsset, err
+
+	if err := vt.checkPlayback(importAsset.ID); err != nil {
+		glog.Errorf("Error checking playback assetId=%s err=%v", importAsset.ID, err)
+		return nil, fmt.Errorf("error checking playback: %w", err)
+	}
+
+	return importAsset, nil
 }
 
 func (vt *vodTester) directUploadTester(fileName string, taskPollDuration time.Duration) error {
@@ -178,8 +185,15 @@ func (vt *vodTester) directUploadTester(fileName string, taskPollDuration time.D
 	err = vt.CheckTaskProcessing(taskPollDuration, uploadTask)
 	if err != nil {
 		glog.Errorf("Error processing asset assetId=%s taskId=%s", uploadAsset.ID, uploadTask.ID)
+		return fmt.Errorf("error waiting for asset processing: %w", err)
 	}
-	return err
+
+	if err := vt.checkPlayback(uploadAsset.ID); err != nil {
+		glog.Errorf("Error checking playback assetId=%s err=%v", uploadAsset.ID, err)
+		return fmt.Errorf("error checking playback: %w", err)
+	}
+
+	return nil
 }
 
 func (vt *vodTester) resumableUploadTester(fileName string, taskPollDuration time.Duration) error {
@@ -217,9 +231,15 @@ func (vt *vodTester) resumableUploadTester(fileName string, taskPollDuration tim
 
 	if err != nil {
 		glog.Errorf("Error processing asset assetId=%s taskId=%s", uploadAsset.ID, uploadTask.ID)
+		return fmt.Errorf("error waiting for asset processing: %w", err)
 	}
 
-	return err
+	if err := vt.checkPlayback(uploadAsset.ID); err != nil {
+		glog.Errorf("Error checking playback assetId=%s err=%v", uploadAsset.ID, err)
+		return fmt.Errorf("error checking playback: %w", err)
+	}
+
+	return nil
 }
 
 func (vt *vodTester) checkPlayback(assetID string) error {
